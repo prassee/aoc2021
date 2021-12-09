46f27576289d08f7803dc7bceb9a0d32adc64ae6
Prasanna <Prasanna>
2021-12-09 08:03:21 +0530

    Revert "initial commit - day1"
    This reverts commit fc25f45bd7a0653d7d5a50ecebd34d89bfe9adbc.

---
+++ day1.go
@@ -0,0 +1,78 @@
+/*
+https://adventofcode.com/2021/day/1
+*/
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+func part1() {
+	file, err := os.Open("day1-input.txt")
+	if err != nil {
+		log.Fatal("cannot find input file ", err.Error())
+	}
+	reader := bufio.NewScanner(file)
+	var prevValue = 0
+	var increments = 0
+	for reader.Scan() {
+		currValue, _ := strconv.Atoi(reader.Text())
+		log.Printf("prev value %v curr value %v is greater %v \n", prevValue, currValue, currValue > prevValue)
+		if currValue > prevValue && prevValue != 0 {
+			increments = increments + 1
+		}
+		prevValue = currValue
+	}
+	log.Printf("no of increments  %v \n", increments)
+}
+
+/*
+same as above but introduces Reducer Struct for the sliding window
+*/
+func part2() {
+	file, err := os.Open("day1-input.txt")
+	if err != nil {
+		log.Fatal("cannot find input file ", err.Error())
+	}
+	reader := bufio.NewScanner(file)
+	reducer := &Reducer{}
+	var prevValue = 0
+	var increments = 0
+	for reader.Scan() {
+		obs, _ := strconv.Atoi(reader.Text())
+		reducer.add(obs)
+		currValue := reducer.reduce()
+		log.Printf("prev value %v curr value %v is greater %v window %v \n", prevValue, currValue, currValue > prevValue, len(reducer.window))
+		if currValue > prevValue && prevValue != 0 {
+			increments = increments + 1
+		}
+		prevValue = currValue
+	}
+	log.Printf("no of increments  %v \n", increments)
+}
+
+type Reducer struct {
+	window []int
+}
+
+func (wind *Reducer) add(a int) {
+	if len(wind.window) == 3 {
+		tail := wind.window[1:]
+		wind.window = append(tail, a)
+	} else {
+		wind.window = append(wind.window, a)
+	}
+}
+
+func (wind *Reducer) reduce() (sum int) {
+	if len(wind.window) == 3 {
+		for _, v := range wind.window {
+			sum = sum + v
+		}
+		return sum
+	}
+	return sum
+}
