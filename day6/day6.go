b76666ae4834530079b9a9f98d81b492c653fa57
Prasanna <Prasanna>
2021-12-15 08:20:31 +0530

    scrapping array for map based idea

--- day6/day6.go
+++ day6/day6.go
@@ -6,40 +6,40 @@ import (
 	"strings"
 )
 
-var days = 256
+var days = 80
+var input = []int{}
+var popDist = make(map[int]int)
 
+/*part 1 & 2 are same except for change in `days`*/
 func Part1() {
-	txtInput := strings.Split("2,3,1,3,4,4,1,5,2,3,1,1,4,5,5,3,5,5,4,1,2,1,1,1,1,1,1,4,1,1,1,4,1,3,1,4,1,1,4,1,3,4,5,1,1,5,3,4,3,4,1,5,1,3,1,1,1,3,5,3,2,3,1,5,2,2,1,1,4,1,1,2,2,2,2,3,2,1,2,5,4,1,1,1,5,5,3,1,3,2,2,2,5,1,5,2,4,1,1,3,3,5,2,3,1,2,1,5,1,4,3,5,2,1,5,3,4,4,5,3,1,2,4,3,4,1,3,1,1,2,5,4,3,5,3,2,1,4,1,4,4,2,3,1,1,2,1,1,3,3,3,1,1,2,2,1,1,1,5,1,5,1,4,5,1,5,2,4,3,1,1,3,2,2,1,4,3,1,1,1,3,3,3,4,5,2,3,3,1,3,1,4,1,1,1,2,5,1,4,1,2,4,5,4,1,5,1,5,5,1,5,5,2,5,5,1,4,5,1,1,3,2,5,5,5,4,3,2,5,4,1,1,2,4,4,1,1,1,3,2,1,1,2,1,2,2,3,4,5,4,1,4,5,1,1,5,5,1,4,1,4,4,1,5,3,1,4,3,5,3,1,3,1,4,2,4,5,1,4,1,2,4,1,2,5,1,1,5,1,1,3,1,1,2,3,4,2,4,3,1", ",")
-	input := []int{}
+	// strings.Split("2,3,1,3,4,4,1,5,2,3,1,1,4,5,5,3,5,5,4,1,2,1,1,1,1,1,1,4,1,1,1,4,1,3,1,4,1,1,4,1,3,4,5,1,1,5,3,4,3,4,1,5,1,3,1,1,1,3,5,3,2,3,1,5,2,2,1,1,4,1,1,2,2,2,2,3,2,1,2,5,4,1,1,1,5,5,3,1,3,2,2,2,5,1,5,2,4,1,1,3,3,5,2,3,1,2,1,5,1,4,3,5,2,1,5,3,4,4,5,3,1,2,4,3,4,1,3,1,1,2,5,4,3,5,3,2,1,4,1,4,4,2,3,1,1,2,1,1,3,3,3,1,1,2,2,1,1,1,5,1,5,1,4,5,1,5,2,4,3,1,1,3,2,2,1,4,3,1,1,1,3,3,3,4,5,2,3,3,1,3,1,4,1,1,1,2,5,1,4,1,2,4,5,4,1,5,1,5,5,1,5,5,2,5,5,1,4,5,1,1,3,2,5,5,5,4,3,2,5,4,1,1,2,4,4,1,1,1,3,2,1,1,2,1,2,2,3,4,5,4,1,4,5,1,1,5,5,1,4,1,4,4,1,5,3,1,4,3,5,3,1,3,1,4,2,4,5,1,4,1,2,4,1,2,5,1,1,5,1,1,3,1,1,2,3,4,2,4,3,1", ",")
+	txtInput := strings.Split("3,4,3,1,2", ",")
+	/*for i := 0; i <= 8; i++ {
+		popDist[i] = 0
+	}*/
 	for _, v := range txtInput {
-		input = append(input, toInt(v))
-	}
-	recurse(1, input)
-	// recurse(1, []int{3, 4, 3, 1, 2})
-}
-
-func recurse(dayCount int, popln []int) {
-	newPop := []int{}
-	for i, v := range popln {
-		newV := v - 1
-		if newV < 0 {
-			newV = 6
-			newPop = append(newPop, 8)
+		x := toInt(v)
+		_, hasKey := popDist[x]
+		if hasKey {
+			popDist[x] = popDist[x] + 1
+		} else {
+			popDist[x] = 1
 		}
-		popln[i] = newV
 	}
-	popln = append(popln, newPop...)
-	if dayCount == days {
-		fmt.Printf("day count %v total popln %v sum population %v \n", dayCount, len(popln), func(x []int) (sum int) {
-			for v := range x {
-				sum += v
-			}
-			return sum
-		}(popln))
-		return
-	} else {
-		recurse(dayCount+1, popln)
-	}
+	fmt.Printf("on day 0 given map is %v \n", popDist)
+	// prevLen := len(input)
+	// for i := 1; i <= days; i++ {
+	// 	for i := 0; i < prevLen; i++ {
+	// 		newV := input[i] - 1
+	// 		if newV < 0 {
+	// 			newV = 6
+	// 			input = append(input, 8)
+	// 		}
+	// 		input[i] = newV
+	// 	}
+	// 	prevLen = len(input)
+	// 	fmt.Printf("on day %v popln %v \n", i, prevLen)
+	// }
 }
 
 func toInt(s string) (i int) {
